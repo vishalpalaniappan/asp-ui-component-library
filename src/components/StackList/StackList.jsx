eeef9ecdb2baf184c4c0745b29f5d34e907b9bb6
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-03-29 19:42:45 -0400

    Add initial implementation of stack trace component.

--- src/components/StackList/StackList.jsx
+++ src/components/StackList/StackList.jsx
@@ -1,7 +1,42 @@
 import "./StackList.scss";
+import PropTypes from 'prop-types';
 
-export const StackList = ({title}) => {
-    return <>
-        <span className="test">{title}</span>
-    </>;
+
+const TraceNode = ({functionName, fileName, lineNumber}) => {
+
+    return (
+        <div className="stackRow">
+            <div className="left">
+                <span className="functionName">{functionName}</span>
+            </div>
+            <div className="right">
+                <span className="fileName">{fileName}</span>
+                <span className="lineNumber">{lineNumber}:1</span>
+            </div>
+        </div>
+    )
+}
+
+export const StackList = ({traces}) => {
+
+    const generateStackList = () => {
+        const traceList = traces.map(trace => 
+            <TraceNode 
+                functionName={trace.functionName}
+                fileName={trace.fileName}
+                lineNumber={trace.lineNumber}             
+            />
+        );
+        return traceList;
+    }
+    
+    return (
+        <div className="stackContainer">
+            {generateStackList()}
+        </div>
+    );
+}
+
+StackList.propTypes = {
+    traces: PropTypes.array
 }
--- src/stories/StackList.stories.js
+++ src/stories/StackList.stories.js
@@ -1,10 +1,35 @@
 import { StackList } from "../components/StackList";
+import "./StackList.scss"
 
 export default {
     title: 'StackList', 
-    component: StackList
+    component: StackList,
+    argTypes: {
+        
+    }
 };
 
-export const DefaultList = {
-    args: {title: "Default List"}
-} 
+const Template = (args) => {
+    return (
+        <div className="rootContainer">
+            <div className="stackContainer">
+                <StackList {...args} /> 
+            </div>
+        </div>
+    )
+}
+
+export const DefaultList = Template.bind({})
+
+DefaultList.args = {
+    traces: [
+        {functionName:"visit_arg", fileName: "helper.py", lineNumber: 3},
+        {functionName:"__init__", fileName: "helper.py", lineNumber: 65},
+        {functionName:"injectLogTypesA", fileName: "LogInjector.py", lineNumber: 3},
+        {functionName:"visit_import", fileName: "LogInjector.py", lineNumber: 3},
+        {functionName:"__init__", fileName: "LogInjector.py", lineNumber: 65},
+        {functionName:"run", fileName: "ProgramProcessor.py", lineNumber: 3},
+        {functionName:"main", fileName: "main.py", lineNumber: 3},
+        {functionName:"<module>", fileName: "main.py", lineNumber: 65}
+    ]
+}
