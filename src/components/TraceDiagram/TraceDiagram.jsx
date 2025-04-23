f16cf0960e20e6a33d38a10c15828eac2d59a733
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-04-23 13:17:48 -0400

    Add styles to flow chart.

--- src/components/TraceDiagram/TraceDiagram.jsx
+++ src/components/TraceDiagram/TraceDiagram.jsx
@@ -13,69 +13,94 @@ import {
   Panel,
   ColorMode,
   Position,
+  MarkerType 
 } from "@xyflow/react";
 
 import "@xyflow/react/dist/style.css";
 
 const nodeDefaults = {
-  sourcePosition: Position.Top,
-  targetPosition: Position.Bottom,
+  sourcePosition: Position.Bottom,
+  targetPosition: Position.Top,
 };
 
 const initialNodes = [
   {
     id: "A",
     type: "input",
-    position: { x: 250, y: 600 },
-    data: { label: "Simulted Client" },
-    ...nodeDefaults,
+    position: { x: 250, y: 0 },
+    data: { label: "Simulted Client" }
   },
   {
     id: "B",
-    position: { x: 250, y: 450 },
-    data: { label: "Job Handler" },
-    ...nodeDefaults,
+    position: { x: 250, y: 150 },
+    data: { label: "Job Handler" }
   },
   {
     id: "C",
     position: { x: 250, y: 300 },
-    data: { label: "Bubble Sort Worker" },
-    ...nodeDefaults,
+    data: { label: "Bubble Sort Worker" }
   },
   {
     id: "D",
-    position: { x: 250, y: 150 },
-    data: { label: "Job Handler" },
-    ...nodeDefaults,
+    position: { x: 250, y: 450 },
+    data: { label: "Job Handler" }
   },
   {
     id: "E",
-    position: { x: 250, y: 0 },
-    data: { label: "Simulated Client" },
-    ...nodeDefaults,
+    type: "output",
+    position: { x: 250, y: 600 },
+    data: { label: "Simulated Client" }
   },
 ];
+
+const marker = {
+  type: MarkerType.ArrowClosed,
+  width: 20,
+  height: 20,
+  color: '#FF0072',
+}
+
+const arrowStyle = {
+  strokeWidth: 2,
+  stroke: '#FF0072',
+}
 
 const initialEdges = [
   {
     id: "A-B",
     source: "A",
     target: "B",
+    animated: true,
+    markerEnd: marker,
+    style: arrowStyle,
+    label: 'Submit Random Job',
   },
   {
     id: "B-C",
     source: "B",
     target: "C",
+    animated: true,
+    markerEnd: marker,
+    style: arrowStyle,
+    label: 'Distribute Storing Job to Worker',
   },
   {
     id: "C-D",
     source: "C",
     target: "D",
+    animated: true,
+    markerEnd: marker,
+    style: arrowStyle,
+    label: 'Return Finished Job to Handler',
   },
   {
     id: "D-E",
     source: "D",
     target: "E",
+    animated: true,
+    markerEnd: marker,
+    style: arrowStyle,
+    label: 'Return Finished Job to Client',
   },
 ];
 
